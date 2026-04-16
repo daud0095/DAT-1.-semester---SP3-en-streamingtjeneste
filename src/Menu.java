04031435785939a9773154aa0387a2ac6bda0e3b
Zouhir <zoel0001@stud.ek.dk>
2026-04-16 13:32:48 +0200

    menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,55 @@
+import util.TextUI;
+
+import java.util.List;
+
 public class Menu {
+    List<User>users;
+    List<Media>media;
+    User currentUser;
+    TextUI textUI;
 
 
+    public Menu(List<User> users, List<Media> media, User currentUser, TextUI textUI) {
+        this.users = users;
+        this.media = media;
+        this.currentUser = currentUser;
+        this.textUI = textUI;
+
+    }
+
+    void start(){
+        if (textUI.promptBinary("1: Har du et login? Y/N")){
+            login();
+        }else{
+            register();
+        }
+
+    }
+    void register(){
+
+    }
+
+    void login(){
+
+    }
+
+    void showMenu(){
+
+    }
+
+    void searchMedia(){
+
+    }
+
+    void searchByCategory(){
+
+    }
+
+    void showWatchedMedia(){
+
+    }
+
+    void showSavedMedia(){
+
+    }
 }
