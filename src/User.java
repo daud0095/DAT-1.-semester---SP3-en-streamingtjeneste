243410a137bf6c6d74da9e97ab3851e66ddad790
daud0095 <daudibragimov2004@gmail.com>
2026-04-16 12:12:12 +0200

    User klasse

--- src/User.java
+++ src/User.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
-}
+        private String username;
+        private String password;
+        private List<Media> watchedMedia;
+        private List<Media> saveMedia;
+
+        public User(String username, String password) {
+            this.username = username;
+            this.password = password;
+            this.watchedMedia = new ArrayList<>();
+            this.saveMedia = new ArrayList<>();
+        }
+
+        public String getUsername(){
+            return username;
+        }
+
+        public String getPassword(){
+            return password;
+        }
+
+        public List<Media> getWatchedMedia(){
+            return watchedMedia;
+        }
+
+        public List<Media> getSaveMedia(){
+            return saveMedia;
+        }
+
+
+    }
+
+
+
