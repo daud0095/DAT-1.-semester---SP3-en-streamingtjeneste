3b436092c834a362f2d82e262c411e0bc0e43407
imanelmou <imel1000@stud.ek.dk>
2026-04-20 13:02:01 +0200

    Movie og media ændret

--- src/Media.java
+++ src/Media.java
@@ -3,17 +3,22 @@ import java.util.List;
 import java.util.Locale;
 
 public abstract class Media {
-    protected String title;
+    protected static String title;
     protected int releaseYear;
     protected double rating;
     protected List<Locale.Category> categories;
 
     public Media(String title, int releaseYear, double rating){
-        this.title = title;
+        Media.title = title;
         this.releaseYear = releaseYear;
         this.rating = rating;
         this.categories = new ArrayList<>();
     }
+
+    public static String getTitle() {
+        return title;
+    }
+
 
     public void play(){
         System.out.println("Afspiller: " + title + " (Udgivet: " + releaseYear + ", Rating: " + rating + ")" );
--- src/Movie.java
+++ src/Movie.java
@@ -1,9 +1,19 @@
 public class Movie extends Media{
     private int duration;
 
-    public Movie(String title, int releaseYear, double rating){
+    public Movie(String title, int releaseYear, double rating) {
         super(title, releaseYear, rating);
     }
 
-
+public void play() {
+    // domain.Movie afspilningslogik her
+    System.out.println("Afspiller film: " + getTitle());
 }
+
+public void pause() {
+    // domain.Movie pauselogik her
+    System.out.println("Film på pause: " + getTitle());
+}
+}
+
+
