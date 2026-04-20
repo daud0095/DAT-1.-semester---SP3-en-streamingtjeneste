dbf92d1bb291b2df75e60e2a3d0af1daf706c710
imanelmou <imel1000@stud.ek.dk>
2026-04-20 11:04:32 +0200

    Onsdag start på opgave.

--- src/Episode.java
+++ src/Episode.java
@@ -1,2 +1,28 @@
 public class Episode {
-}
+
+        private int episodeNumber;
+        private int duration;
+
+    public Episode (int episodeNumber, int duration) {
+        this.episodeNumber = episodeNumber;
+        this.duration = duration;
+    }
+
+    public void play() {
+            System.out.println("Playing episode: " + episodeNumber); // f.eks. ep.1
+
+        }
+
+        public void pause() {
+            System.out.println("domain.Episode paused: " + episodeNumber);
+        }
+
+        public int getDuration(){
+            System.out.println("This ep. duration is: " + duration);
+             return duration;
+
+        }
+
+    }
+
+
