f86a8dc6e66e288d5547dbd96510e1bd51071466
Zouhir <zoel0001@stud.ek.dk>
2026-04-20 10:51:44 +0200

    Serie

--- src/Series.java
+++ src/Series.java
@@ -1,2 +1,11 @@
+import java.util.List;
+
 public class Series extends Media{
+
+    private String series;
+    List<Season> seasons;
+    public Series(String series){
+        super("Stranger Things", 2017, 9.0 );
+        this.series = series;
+    }
 }
