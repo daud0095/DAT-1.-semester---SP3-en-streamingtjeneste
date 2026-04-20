9b60f839fe403930054373d5c3974a1bcceda525
imanelmou <imel1000@stud.ek.dk>
2026-04-20 12:29:24 +0200

    Onsdag start på opgave.

--- src/Movie.java
+++ src/Movie.java
@@ -2,7 +2,7 @@ public class Movie extends Media{
     private int duration;
 
     public Movie(String title, int releaseYear, double rating){
-        super(title, releaseYear, rating);
+        super(title, releaseYear, rating, categories);
     }
 
     public int getDuration(){
--- src/Season.java
+++ src/Season.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Season extends Series {
@@ -8,6 +9,22 @@ public class Season extends Series {
     public Season(int seasonNumber) {
         super("5");
         this.seasonNumber = seasonNumber;
+    }
+
+    public int getSeasonNumber() {
+        return seasonNumber;
+    }
+
+    public void setSeasonNumber(int seasonNumber) {
+        this.seasonNumber = seasonNumber;
+    }
+
+    public ArrayList<Episode> getEpisodes() {
+        return (ArrayList<Episode>) episodes;
+    }
+
+    public void setEpisode(ArrayList<Episode> episodes) {
+        this.episodes = episodes;
     }
 
 
