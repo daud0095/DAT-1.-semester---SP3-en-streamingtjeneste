781daef06a524ef1de39bb149cc3af1c39236f9e
imanelmou <imel1000@stud.ek.dk>
2026-04-20 11:04:32 +0200

    Onsdag start på opgave.

---
+++ src/Category.java
@@ -0,0 +1,23 @@
+public enum Category {
+
+        CRIME,
+        WAR,
+        DRAMA,
+        FAMILY,
+        ROMANCE,
+        SCI_FI,
+        FANTASY,
+        MYSTERY,
+        THRILLER,
+        ADVENTURE,
+        HISTORY,
+        BIOGRAPHY,
+        HORROR,
+        ANIMATION,
+        ACTION,
+        WESTERN,
+        DOCUMENTARY,
+        TALK_SHOW,
+        SPORT,
+
+}
